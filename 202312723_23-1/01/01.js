54a4c584abf4935c650eddff4b9119da6cb26d5d
dsk0903 <51107940+dsk0903@users.noreply.github.com>
2023-04-21 22:24:52 +0900

    commit message

---
+++ 202312723_23-1/01/01.js
@@ -0,0 +1,6 @@
+const answer = '노드는 단일 스레드 방식으로 하나의 스레드가 여러가지 일을 처리한다.+\
+ 하나의 일을 처리하고 그 이후에 대기중이던 다른일을 처리하는 방식으로 이루어진다.+\
+ node js가 멀티 스레드 방식을 지원하는 방법은 +\
+ Child Process 모듈 사용하기, Cluster 모듈 사용하기, +\
+ Worker Threads 모듈 사용하기 등의 방법으로 지원 가능하다.';
+console.log(answer);
